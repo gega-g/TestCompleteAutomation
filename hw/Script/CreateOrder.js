10659d8ae18b75699d19b73451fa7292cba6c182
gega-g <132761174+gega-g@users.noreply.github.com>
2024-05-31 23:26:13 +0400

    Add files via upload

---
+++ hw/Script/CreateOrder.js
@@ -0,0 +1,29 @@
+﻿var orderAppSteps = require("OrderAppSteps");
+
+function CreateOrder1()
+{
+  orderAppSteps.openOrder()
+  Aliases.Orders.OrderForm.Group.Customer.SetText("User1")
+  Aliases.Orders.OrderForm.Group.Street.SetText("Street1")
+  Aliases.Orders.OrderForm.Group.City.SetText("Kutaisi")
+  Aliases.Orders.OrderForm.ButtonOK.Click()
+}
+function CreateOrder2()
+{                          
+  orderAppSteps.openOrder()
+  Aliases.Orders.OrderForm.Group.Customer.SetText("User2")
+  Aliases.Orders.OrderForm.Group.State_.SetText("Samegrelo")
+  Aliases.Orders.OrderForm.Group.City.SetText("Walenjixa")
+  Aliases.Orders.OrderForm.ButtonOK.Click()
+}
+function CreateOrder3()
+{
+  orderAppSteps.openOrder()
+  Aliases.Orders.OrderForm.Group.Customer.SetText("User3")
+  Aliases.Orders.OrderForm.Group.Zip.SetText("40125")
+  Aliases.Orders.OrderForm.Group.CardNo.SetText("123467890")
+  Aliases.Orders.OrderForm.ButtonOK.Click()
+}
+module.exports.CreateOrder1 = CreateOrder1;
+module.exports.CreateOrder2 = CreateOrder2;
+module.exports.CreateOrder3 = CreateOrder3;
---
+++ hw/Script/OrderTests.js
@@ -0,0 +1,11 @@
+﻿var appSteps = require("OrderAppSteps");
+var orderSteps = require("CreateOrder");
+
+function createOrderWithTwoUsers()
+{
+  appSteps.openApp();
+  orderSteps.CreateOrder1();
+  orderSteps.CreateOrder2();
+  orderSteps.CreateOrder3();
+  appSteps.close();
+}
